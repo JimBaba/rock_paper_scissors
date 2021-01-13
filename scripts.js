bb2646d478f089bed8a322f0fb4b6ee8f35cf1a9
JimBaba <aalberts.daniel@gmail.com>
2021-01-13 19:02:53 +0100

    final version incl. 5 round game()

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
-let playerSelection = prompt("Rock, Paper or Scissors?")
-playerSelection = playerSelection.toLowerCase()
-let computerSelection = computerPlay()
+
+    let playerScore = 0;
+    let cpuScore = 0;
+    let numberOfGames = 5;
+
 
 function computerPlay(){
     let rnd = Math.floor(Math.random() *10);
@@ -14,13 +16,47 @@ function computerPlay(){
     
 }
 
-function playRound(playerSelection, computerSelection){
+
+
+
+function playRound(){
+    let playerSelection = prompt("Rock, Paper or Scissors?")
+    playerSelection = playerSelection.toLowerCase()
+    let computerSelection = computerPlay()
+
 
     if(playerSelection == computerSelection){
-        return "Tie! Try again"
+        console.log("Tie! Try again")
     } else if(playerSelection == "rock" && computerSelection == "paper"){
-        return ()
+        cpuScore += 1;
+        console.log(playerSelection + " loses against " + computerSelection + "! " + " The score is: " + "Player: " + playerScore + " Computer: " + cpuScore)
+    } else if(playerSelection == "rock" && computerSelection == "scissors"){
+        playerScore += 1;
+        console.log("You win " + playerSelection + " beats " + computerSelection + "! " + " The score is: " + "Player: " + playerScore + " Computer: " + cpuScore)
+    } else if(playerSelection == "paper" && computerSelection == "rock"){
+        playerScore += 1;
+        console.log("You win " + playerSelection + " beats " + computerSelection + "! " + " The score is: " + "Player: " + playerScore + " Computer: " + cpuScore)
+    } else if(playerSelection == "paper" && computerSelection == "scissors"){
+        cpuScore += 1;
+        console.log(playerSelection + " loses against " + computerSelection + "! " + " The score is: " + "Player: " + playerScore + " Computer: " + cpuScore)
+    } else if(playerSelection == "scissors" && computerSelection == "paper"){
+        playerScore += 1;
+        console.log("You win " + playerSelection + " beats " + computerSelection + "! " + " The score is: " + "Player: " + playerScore + " Computer: " + cpuScore)
+    } else {
+        cpuScore += 1;
+        console.log(playerSelection + " loses against " + computerSelection + "! " + " The score is: " + "Player: " + playerScore + " Computer: " + cpuScore)
     }
 
+
+}
+
+function game(){
+
+    for(let i = 0; i < numberOfGames; i++){
+        playRound();
+    }
+    console.log("Player: " + playerScore + " - CPU: " + cpuScore)
+    playerScore = 0;
+    cpuScore = 0;
 }
 
