df64cbad04f847832290520a3a114e996580c10f
Daniel Aalberts <aalberts.daniel@gmail.com>
2021-01-13 15:44:55 +0100

    added playRound(), not finished

--- scripts.js
+++ scripts.js
@@ -1,11 +1,26 @@
+let playerSelection = prompt("Rock, Paper or Scissors?")
+playerSelection = playerSelection.toLowerCase()
+let computerSelection = computerPlay()
+
 function computerPlay(){
     let rnd = Math.floor(Math.random() *10);
     if(rnd <= 3){
-        return "Rock"
+        return "rock"
     } else if(rnd > 3 && rnd <= 6){
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
     
 }
+
+function playRound(playerSelection, computerSelection){
+
+    if(playerSelection == computerSelection){
+        return "Tie! Try again"
+    } else if(playerSelection == "rock" && computerSelection == "paper"){
+        return ()
+    }
+
+}
+
